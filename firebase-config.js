51b324750ab130abdd9ae09c05f63843860b4b1f
YadavAnkit-30 <ankit93598@gmail.com>
2025-11-26 16:31:21 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,14 +3,15 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-aut
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBLWXsntvHFRmn-w_665gJexiYp7zmC-og",
-  authDomain: "dailychronicle-a9ce1.firebaseapp.com",
-  projectId: "dailychronicle-a9ce1",
+  apiKey: "YOUR API KEY",
+  authDomain: "",
+  projectId: "",
   storageBucket: "dailychronicle-a9ce1.appspot.com", 
-  messagingSenderId: "896770911755",
-  appId: "1:896770911755:web:08eba447dbfc2e4a57610e"
+  messagingSenderId: "",
+  appId: "YOUR appid"
 };
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
